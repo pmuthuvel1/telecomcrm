2a6f55c3ebc373f5f2595b66fde7ea2475558c8e
pmuthuvel1 <pmuthuvel1@gmail.com>
2019-11-02 23:00:49 +0400

    init2

---
+++ src/main/java/com/crm/telecomcrm/TelecomcrmApplication.java
@@ -0,0 +1,13 @@
+package com.crm.telecomcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomcrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomcrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/telecomcrm/TelecomcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.telecomcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomcrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
